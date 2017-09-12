d2f095fc864ff3c0429d2f88b61d67c7613b71cc
Progyan Bhattacharya <progyanb@acm.org>
2017-09-12 09:45:38 +0530

    12-09-2017: Matrix Chain Multiplication
    Finding minimum cost for matrix chain multiplication using dynamic programming.
    
    Signed-off-by: Progyan Bhattacharya <progyanb@acm.org>

---
+++ Dynamic Programming/Matrix Multpilication/main.c
@@ -0,0 +1,26 @@
+/*
+ * Matrix Chain Multiplication
+ * Author: Progyan Bhattacharya <progyanb@acm.org>
+ * Repo: Design-And-Analysis-of-Algorithm [MIT LICENSE]
+ */
+
+#include "MatMul.h"
+
+int main(int argc, char** argv) {
+    if (argc < 2) {
+        fprintf(stderr, "Usage: %s [n] [n integers]\n", argv[0]);
+        return EXIT_FAILURE;
+    }
+    int n = atoi(argv[1]);
+    if (argc != (n + 2)) {
+        fprintf(stderr, "Fatal Error: Invalid number of integer supplied\n");
+        return EXIT_FAILURE;
+    }
+    int A[n], nmul;
+    for (int i = 0; i < n; i++) {
+        A[i] = atoi(argv[i + 2]);
+    }
+    nmul = matMul(A, n);
+    printf("Total possible number of multiplication is: %d\n", nmul);
+    return EXIT_SUCCESS;
+}
