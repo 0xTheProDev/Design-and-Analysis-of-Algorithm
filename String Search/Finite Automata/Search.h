141ebe9bd8bf72b40f641a270ad03cf0267650ef
Progyan Bhattacharya <progyanb@acm.org>
2017-10-20 15:44:36 +0530

    String matching problem: Finite Automata
    Search first occurence of a particular string in a given text

---
+++ String Search/Finite Automata/Search.h
@@ -0,0 +1,25 @@
+/*
+ * Search first occurence of a particular string in a given text [Finite Automata]
+ * Author: Progyan Bhattacharya <progyanb@acm.org>
+ * Repo: Design-And-Analysis-of-Algorithm [MIT LICENSE]
+ */
+
+#ifndef __Search_h
+#define __Search_h
+
+#ifndef __limits_h
+#include <limits.h>
+#endif
+#ifndef __stdio_h
+#include <stdio.h>
+#endif
+#ifndef __stdlib_h
+#include <stdlib.h>
+#endif
+
+/**
+ * Proto type for Search function
+ */
+int Search(int, char*, int, char*);
+
+#endif
---
+++ String Search/Finite Automata/main.c
@@ -0,0 +1,21 @@
+/*
+ * Search first occurence of a particular string in a given text [Finite Automata]
+ * Author: Progyan Bhattacharya <progyanb@acm.org>
+ * Repo: Design-And-Analysis-of-Algorithm [MIT LICENSE]
+ */
+
+#include "Search.h"
+
+int main(int argc, char const *argv[]) {
+    if (argc != 3) {
+        fprintf(stderr, "Usage: %s [n] [m]\nn: Length of text  m: Length of search string", argv[0]);
+        return EXIT_FAILURE;
+    }
+    int n = atoi(argv[1]);
+    int m = atoi(argv[2]);
+    char text[n], str[m];
+    scanf("%s%*[\n]%s", text, str);
+    int index = Search(n, text, m, str);
+    printf("Search string found at: %d (0-indexed)\n", index);
+    return EXIT_SUCCESS;
+}
